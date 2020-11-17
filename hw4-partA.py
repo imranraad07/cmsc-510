5374adb9a85d3b4acc135e4130f05f267e5271f4
imranm3 <imranm3@egr-u-cs-kd-03.rams.adp.vcu.edu>
2020-11-16 21:41:05 -0500

    added

--- hw4-partA.py
+++ hw4-partA.py
@@ -28,13 +28,15 @@ class Model(nn.Module):
         super(Model, self).__init__()
         self.layer1 = nn.Sequential(
             nn.Conv2d(1, hidden_sizes[0], kernel_size=5, stride=1, padding=2), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2))
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
         self.layer2 = nn.Sequential(
             nn.Conv2d(hidden_sizes[0], hidden_sizes[1], kernel_size=5, stride=1, padding=2),
-            nn.ReLU(), nn.MaxPool2d(kernel_size=2, stride=2))
+            nn.ReLU(), nn.MaxPool2d(kernel_size=2, stride=2),
+        )
         self.drop_out = nn.Dropout()
         self.fc1 = nn.Linear(7 * 7 * 16, 1024)
-        self.fc2 = nn.Linear(1024, output_size)
+        self.fc2 = nn.ReLU(1024)
 
     def forward(self, x):
         out = self.layer1(x)
@@ -46,14 +48,59 @@ class Model(nn.Module):
         return out
 
 
+class ModelBN(nn.Module):
+    def __init__(self):
+        super(ModelBN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, hidden_sizes[0], kernel_size=5, stride=1, padding=2), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(hidden_sizes[0])
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(hidden_sizes[0], hidden_sizes[1], kernel_size=5, stride=1, padding=2),
+            nn.ReLU(), nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(hidden_sizes[1])
+        )
+        self.drop_out = nn.Dropout()
+        self.fc1 = nn.Linear(7 * 7 * 16, 1024)
+        self.fc2 = nn.ReLU(1024)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.drop_out(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
+
+
+modelBN = ModelBN()
+criterionBN = torch.nn.CrossEntropyLoss()
+optimizerBN = optim.Adam(modelBN.parameters(), lr=learning_rate)
+
+time0 = time()
+epochs = 15
+iteration = []
+lossesBN = []
+for epoch in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+        optimizerBN.zero_grad()
+        outputs = modelBN(images)
+        loss = criterionBN(outputs, labels)
+        loss.backward()
+        optimizerBN.step()
+        running_loss += loss.item()
+    print("Epoch {} - Training loss: {}".format(epoch, running_loss / len(trainloader)))
+    lossesBN.append(running_loss)
+    iteration.append(epoch)
+
 model = Model()
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-time0 = time()
-epochs = 15
 losses = []
-iteration = []
 for epoch in range(epochs):
     running_loss = 0
     for images, labels in trainloader:
@@ -65,21 +112,22 @@ for epoch in range(epochs):
         running_loss += loss.item()
     print("Epoch {} - Training loss: {}".format(epoch, running_loss / len(trainloader)))
     losses.append(running_loss)
-    iteration.append(epoch)
 
 print("Training Time (in minutes) =", (time() - time0) / 60)
 
-plt.plot(iteration, losses, color='red')
+plt.plot(iteration, lossesBN, color='red', label='Batch Norm')
+plt.plot(iteration, losses, color='blue', label='Without Batch Norm')
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Loss rate vs. iterations of training dataset (pytorch)')
+plt.legend()
 plt.show()
 
 correct_count, all_count = 0, 0
 
 with torch.no_grad():
     for images, labels in test_loader:
-        outputs = model(images)
+        outputs = modelBN(images)
         _, predicted = torch.max(outputs.data, 1)
         all_count += labels.size(0)
         correct_count += (predicted == labels).sum()
--- hw4-partB.py
+++ hw4-partB.py
@@ -1,7 +1,7 @@
 # /**
 #  * @author Mia Mohammad Imran
 #  */
-
+from abc import ABC
 from time import time
 import tensorflow as tf
 from tensorflow.keras import Model, layers
@@ -23,7 +23,7 @@ train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
 train_data = train_data.repeat().shuffle(5000).batch(batch_size).prefetch(1)
 
 
-class Model(Model):
+class Model(Model, ABC):
     def __init__(self):
         super(Model, self).__init__()
         self.conv1 = layers.Conv2D(hidden_sizes[0], kernel_size=5, activation=tf.nn.relu)
@@ -36,16 +36,16 @@ class Model(Model):
         self.out = layers.Dense(output_size)
 
     def call(self, x, **kwargs):
-        out = tf.reshape(x, [-1, 28, 28, 1])
-        out = self.conv1(out)
-        out = self.maxpool1(out)
-        out = self.conv2(out)
-        out = self.maxpool2(out)
-        out = self.flatten(out)
-        out = self.fc1(out)
-        out = self.dropout(out)
-        out = self.out(out)
-        return out
+        x = tf.reshape(x, [-1, 28, 28, 1])
+        x = self.conv1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.maxpool2(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.dropout(x)
+        x = self.out(x)
+        return x
 
 
 cnn = Model()
@@ -60,15 +60,63 @@ cnn.compile(
     metrics=["accuracy"],
 )
 
-history = cnn.fit(x_train, y_train, batch_size=batch_size, epochs=15)
+history = cnn.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
+
+
+class ModelBN(Model, ABC):
+    def __init__(self):
+        super(ModelBN, self).__init__()
+        self.conv1 = layers.Conv2D(hidden_sizes[0], kernel_size=5, activation=tf.nn.relu)
+        self.bn1 = layers.BatchNormalization()
+        self.maxpool1 = layers.MaxPool2D(2, strides=2)
+        self.conv2 = layers.Conv2D(hidden_sizes[1], kernel_size=5, activation=tf.nn.relu)
+        self.bn2 = layers.BatchNormalization()
+        self.maxpool2 = layers.MaxPool2D(2, strides=2)
+        self.flatten = layers.Flatten()
+        self.fc1 = layers.Dense(1024)
+        self.bn3 = layers.BatchNormalization()
+        self.dropout = layers.Dropout(rate=0.5)
+        self.out = layers.Dense(output_size)
+        self.bn4 = layers.BatchNormalization()
+
+    def call(self, x, **kwargs):
+        x = tf.reshape(x, [-1, 28, 28, 1])
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.maxpool2(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.bn3(x)
+        x = self.dropout(x)
+        x = self.out(x)
+        x = self.bn4(x)
+        return x
+
+
+cnnBN = ModelBN()
+optimizerBN = tf.optimizers.Adam(learning_rate)
+
+time0 = time()
+
+cnnBN.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=optimizerBN,
+    metrics=["accuracy"],
+)
+
+historyBN = cnnBN.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test))
 
 print("Training Time (in minutes) =", (time() - time0) / 60)
-
-plt.plot(history.epoch, history.history['loss'], color='blue')
+plt.plot(historyBN.epoch, historyBN.history['loss'], color='red', label='Batch Norm')
+plt.plot(history.epoch, history.history['loss'], color='blue', label='Without Batch Norm')
 plt.xlabel('x')
 plt.ylabel('y')
+plt.legend()
 plt.title('Loss rate vs. iterations of training dataset (tensorflow)')
 plt.show()
 
-pred = cnn.fit((x_test), y_test)
-print("Model Accuracy = ", pred.history['accuracy'])
+# 0.9909
+# 0.9909
